5f6bb7f1004584378ebf0b94637d133b549f1972
Fabien BERNARD <fabien0102@hotmail.com>
2022-02-22 10:20:58 +0100

    feat: add JSDocTag filter (#72)

---
+++ src/utils/getSimplifiedJsDocTags.ts
@@ -0,0 +1,22 @@
+import ts from "typescript";
+import { SimplifiedJSDocTag } from "../config";
+
+/**
+ * Get a simplified version of a node JSDocTags.
+ *
+ * @param jsDocs
+ */
+export function getSimplifiedJsDocTags(
+  jsDocs: ts.JSDoc[]
+): SimplifiedJSDocTag[] {
+  const tags: SimplifiedJSDocTag[] = [];
+  jsDocs.forEach((jsDoc) => {
+    (jsDoc.tags || []).forEach((tag) => {
+      const name = tag.tagName.escapedText.toString();
+      const value = typeof tag.comment === "string" ? tag.comment : undefined;
+      tags.push({ name, value });
+    });
+  });
+
+  return tags;
+}
