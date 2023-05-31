22b71f38ff214082fbe7b0751655a287b37f7b9f
tvillaren <tvillaren@users.noreply.github.com>
2023-05-31 18:03:46 +0200

    refacto: no need for recursive extract

--- src/utils/traverseTypes.ts
+++ src/utils/traverseTypes.ts
@@ -16,56 +16,37 @@ export function isTypeNode(node: ts.Node): node is TypeNode {
 
 export function getExtractedTypeNames(
   node: TypeNode,
-  sourceFile: ts.SourceFile,
-  typeNameMapping: Map<string, TypeNode>
+  sourceFile: ts.SourceFile
 ) {
   const referenceTypeNames: string[] = [];
 
-  const recursiveExtract = (node: TypeNode) => {
-    if (node.visited) {
+  if (node.visited) {
+    return;
+  }
+
+  const heritageClauses = (node as ts.InterfaceDeclaration).heritageClauses;
+
+  if (heritageClauses) {
+    heritageClauses.forEach((clause) => {
+      const extensionTypes = clause.types;
+      extensionTypes.forEach((extensionTypeNode) => {
+        const typeName = extensionTypeNode.expression.getText(sourceFile);
+
+        referenceTypeNames.push(typeName);
+      });
+    });
+  }
+
+  node.forEachChild((child) => {
+    const childNode = child as ts.PropertySignature;
+    if (childNode.kind !== ts.SyntaxKind.PropertySignature) {
       return;
     }
 
-    const heritageClauses = (node as ts.InterfaceDeclaration).heritageClauses;
-
-    if (heritageClauses) {
-      heritageClauses.forEach((clause) => {
-        const extensionTypes = clause.types;
-        extensionTypes.forEach((extensionTypeNode) => {
-          const typeName = extensionTypeNode.expression.getText(sourceFile);
-          const typeNode = typeNameMapping.get(typeName);
-
-          referenceTypeNames.push(typeName);
-
-          if (typeNode) {
-            typeNode.visited = true;
-            recursiveExtract(typeNode);
-          }
-        });
-      });
+    if (childNode.type?.kind === ts.SyntaxKind.TypeReference) {
+      referenceTypeNames.push(childNode.type.getText(sourceFile));
     }
+  });
 
-    node.forEachChild((child) => {
-      const childNode = child as ts.PropertySignature;
-      if (childNode.kind !== ts.SyntaxKind.PropertySignature) {
-        return;
-      }
-
-      if (childNode.type?.kind === ts.SyntaxKind.TypeReference) {
-        const typeNode = typeNameMapping.get(
-          childNode.type.getText(sourceFile)
-        );
-
-        referenceTypeNames.push(childNode.type.getText(sourceFile));
-
-        if (typeNode) {
-          typeNode.visited = true;
-          recursiveExtract(typeNode);
-        }
-      }
-    });
-  };
-
-  recursiveExtract(node);
   return [node.name.text, ...referenceTypeNames];
 }
