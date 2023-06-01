e8407a4bc0bfdbc68a3a672f7e681dfb1d6a56df
Thomas V <2619415+tvillaren@users.noreply.github.com>
2023-06-01 17:21:32 +0200

    test: implement basic test cases

---
+++ src/utils/traverseTypes.test.ts
@@ -0,0 +1,91 @@
+import ts from "typescript";
+import { getExtractedTypeNames } from "./traverseTypes";
+import { findNode } from "./findNode";
+
+describe("traverseTypes", () => {
+  describe("getExtractedTypeNames", () => {
+    it("should find only itself", () => {
+      const testCases = [
+        `
+        export type Superhero = {
+            id: number, 
+            name: string
+          };
+        `,
+        `
+        export interface Superhero = {
+            id: number, 
+            name: string
+          };
+        `,
+        `
+        export enum Superhero = {
+            Superman = "superman",
+            ClarkKent = "clark_kent",
+          };
+        `,
+      ];
+
+      testCases.forEach((source: string) => {
+        const result = extractNames(source);
+        expect(result).toEqual(["Superhero"]);
+      });
+    });
+
+    it("should extract type referenced in property", () => {
+      const source = `
+        export interface Superhero {
+            id: number,
+            person: Person,
+        }`;
+
+      const result = extractNames(source);
+      expect(result).toEqual(["Superhero", "Person"]);
+    });
+
+    it("should extract type referenced in extend clause", () => {
+      const source = `
+          export interface Superhero extends Person {
+              id: number,
+          }`;
+
+      const result = extractNames(source);
+      expect(result).toEqual(["Superhero", "Person"]);
+    });
+
+    it("should extract type referenced in multiple extend clauses", () => {
+      const source = `
+            export interface Superhero extends Person, Person2 {
+                id: number,
+            }`;
+
+      const result = extractNames(source);
+      expect(result).toEqual(["Superhero", "Person", "Person2"]);
+    });
+  });
+});
+
+function extractNames(sourceText: string) {
+  const sourceFile = ts.createSourceFile(
+    "index.ts",
+    sourceText,
+    ts.ScriptTarget.Latest
+  );
+  const declaration = findNode(
+    sourceFile,
+    (
+      node
+    ): node is
+      | ts.InterfaceDeclaration
+      | ts.TypeAliasDeclaration
+      | ts.EnumDeclaration =>
+      ts.isInterfaceDeclaration(node) ||
+      ts.isTypeAliasDeclaration(node) ||
+      ts.isEnumDeclaration(node)
+  );
+  if (!declaration) {
+    throw new Error("No `type` or `interface` found!");
+  }
+
+  return getExtractedTypeNames(declaration, sourceFile);
+}
