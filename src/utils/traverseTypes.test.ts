276c16e55d7274bf3c9514e3069a3fcf0848d9a9
Thomas V <2619415+tvillaren@users.noreply.github.com>
2023-09-06 10:27:40 +0200

    fix: typo

--- src/utils/traverseTypes.test.ts
+++ src/utils/traverseTypes.test.ts
@@ -176,7 +176,7 @@ describe("traverseTypes", () => {
 
     it("should extract type from type alias with object literal", () => {
       const source = `
-        export type Person = { hero: SuperHero} `;
+        export type Person = { hero: SuperHero } `;
 
       const result = extractNames(source);
       expect(result).toEqual(["Person", "SuperHero"]);
@@ -184,7 +184,7 @@ describe("traverseTypes", () => {
 
     it("should extract type from type alias with union & object literal", () => {
       const source = `
-        export type Person = Villain | { hero: SuperHero} `;
+        export type Person = Villain | { hero: SuperHero } `;
 
       const result = extractNames(source);
       expect(result).toEqual(["Person", "Villain", "SuperHero"]);
