a1bc18586bdefbd5f35c1a4969d4f9c2e6fcfa07
Thomas V <2619415+tvillaren@users.noreply.github.com>
2024-02-19 12:08:05 +0100

    fix: test not passing in Windows env

--- src/utils/countNetParentDirectoriesInRelativePath.test.ts
+++ src/utils/countNetParentDirectoriesInRelativePath.test.ts
@@ -1,3 +1,4 @@
+import { sep } from "path";
 import { countNetParentDirectoriesInRelativePath } from "./countNetParentDirectoriesInRelativePath";
 
 describe("countNetUpFoldersInRelativePath", () => {
@@ -6,19 +7,23 @@ describe("countNetUpFoldersInRelativePath", () => {
   });
 
   it("should return 0 for a path with no parent directory", () => {
-    expect(countNetParentDirectoriesInRelativePath("folder/file.txt")).toBe(0);
+    expect(
+      countNetParentDirectoriesInRelativePath(`folder${sep}file.txt`)
+    ).toBe(0);
   });
 
   it("should return 1 for a path with one parent directory", () => {
     expect(
-      countNetParentDirectoriesInRelativePath("../folder1/folder2/file.txt")
+      countNetParentDirectoriesInRelativePath(
+        `..${sep}folder1${sep}folder2${sep}file.txt`
+      )
     ).toBe(1);
   });
 
   it("should return 2 for a path with two parent directories", () => {
     expect(
       countNetParentDirectoriesInRelativePath(
-        "../../folder1/folder2/folder3/file.txt"
+        `..${sep}..${sep}folder1${sep}folder2${sep}folder3${sep}file.txt`
       )
     ).toBe(2);
   });
@@ -26,7 +31,7 @@ describe("countNetUpFoldersInRelativePath", () => {
   it("should return 2 for a path with two parent directories not at the beginining", () => {
     expect(
       countNetParentDirectoriesInRelativePath(
-        "../folder1/../../folder2/folder3/file.txt"
+        `..${sep}folder1${sep}..${sep}..${sep}folder2${sep}folder3${sep}file.txt`
       )
     ).toBe(2);
   });
@@ -34,7 +39,7 @@ describe("countNetUpFoldersInRelativePath", () => {
   it("should return 3 for a path with 3 parent directories not at the beginining with more folders in between", () => {
     expect(
       countNetParentDirectoriesInRelativePath(
-        "../folder1/folder2/../../../..//folder3/file.txt"
+        `..${sep}folder1${sep}folder2${sep}..${sep}..${sep}..${sep}..${sep}${sep}folder3${sep}file.txt`
       )
     ).toBe(3);
   });
